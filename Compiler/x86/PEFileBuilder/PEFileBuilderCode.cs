f377cc1b238fc7a2cc3aedf3897f6c8acff57de0
Shay Ben Moshe <shayfalador@gmail.com>
2017-03-11 09:59:45 +0200

    some more

--- Compiler/x86/PEFileBuilder/PEFileBuilderCode.cs
+++ Compiler/x86/PEFileBuilder/PEFileBuilderCode.cs
@@ -5,32 +5,24 @@ namespace Compiler
 {
     public partial class PEFileBuilder
     {
-        private uint codeRVA = 0x1000;
-        
-        private uint codeOffset;
-        
-        List<byte> code;
-        private uint entryPointInCode;
-        
         public void EmitCode(List<byte> code, uint entryPointInCode)
         {
-            this.code = code;
-            this.entryPointInCode = entryPointInCode;
+            int codeRVA = 0x1000;
 
-            this.codeOffset = (uint)this.output.Count;
-            this.output.AddRange(this.code);
+            int codeOffset = this.output.Count;
+            this.output.AddRange(code);
             this.AlignToSectionAlignment();
             int afterCode = this.output.Count;
 
-            uint codeSize = (uint)(afterCode - this.codeOffset);
+            uint codeSize = (uint)(afterCode - codeOffset);
 
-            this.valuesFixers[ValuesFixerKeys.SizeOfImage].Value = this.codeRVA + codeSize;
+            this.valuesFixers[ValuesFixerKeys.SizeOfImage].Value = (uint)(codeRVA + codeSize);
 
-            this.valuesFixers[ValuesFixerKeys.CodeVirtualSize].Value = (uint)this.code.Count;
-            this.valuesFixers[ValuesFixerKeys.CodeVirtualOffset].Value = this.codeRVA;
-            this.valuesFixers[ValuesFixerKeys.EntryPoint].Value = this.codeRVA + this.entryPointInCode;
-            this.valuesFixers[ValuesFixerKeys.CodeDiskSize].Value = (uint)(afterCode - this.codeOffset);
-            this.valuesFixers[ValuesFixerKeys.CodeRVA].Value = this.codeOffset;
+            this.valuesFixers[ValuesFixerKeys.CodeVirtualSize].Value = (uint)code.Count;
+            this.valuesFixers[ValuesFixerKeys.CodeVirtualOffset].Value = (uint)codeRVA;
+            this.valuesFixers[ValuesFixerKeys.EntryPoint].Value = (uint)(codeRVA + entryPointInCode);
+            this.valuesFixers[ValuesFixerKeys.CodeDiskSize].Value = (uint)(afterCode - codeOffset);
+            this.valuesFixers[ValuesFixerKeys.CodeRVA].Value = (uint)codeOffset;
         }
     }
 }
